eab9e465d4514994b38a8ba85a03a75113bb3af8
Balaji SR <balaji.srb@gmail.com>
2021-09-13 22:27:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
-#import haslib
 
 @app.route("/")
 def hello():
+    import haslib
     #print (hashlib.algorithms_available)
     return "Hello, World!!!"
    
